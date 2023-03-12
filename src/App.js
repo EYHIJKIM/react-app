64d2170565cb2a3234cdf7ec46219f93b8ea7ff8
kimze <crk33333@gmail.com>
2023-03-12 20:35:21 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//사용자 정의 태그 = 컴포넌트
+function Header(){ //react에서는 반드시 컴포넌트는 대문자 시작
+return <header>
+          <h1><a href="/">WEB</a></h1>
+        </header>
+
+}
+
+function Nav(){
+  return <nav>
+  <ol>
+    <li><a href="/read/1">html</a></li>
+    <li><a href="/read/2">css</a></li>
+    <li><a href="/read/3">js</a></li>
+  </ol>
+</nav>
+}
+
+function Article(){
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB
+</article>
+
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header></Header> 
+        <Nav></Nav>
+        <Article></Article>
     </div>
+  
   );
 }
 
